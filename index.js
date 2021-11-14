4e80729b5f8cbc4d75de578390c94e093859b3b0
steph <sjapodaca1103@gmail.com>
2021-11-13 16:30:34 -0800

    added more to inquirer and deleted HTML add on in js

--- index.js
+++ index.js
@@ -1,67 +1,51 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const Choices = require('inquirer/lib/objects/choices');
+const userInfo = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the name of your project?',
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Provide a short description of your project',
+        },
 
-const generateHTML = ({ name, location, github, linkedin }) =>
-    `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
 
-inquirer
-    .prompt([
         {
             type: 'input',
-            name: 'name',
-            message: 'What is your name?',
+            name: 'Installation',
+            message: 'What are the steps to install your project?',
         },
         {
             type: 'input',
-            name: 'location',
-            message: 'Where are you from?',
+            name: 'Usage',
+            message: 'Provide instructions and examples for use',
         },
         {
             type: 'input',
-            name: 'hobby',
-            message: 'What is your favorite hobby?',
+            name: 'Credits',
+            message: 'List your collaborators, if any with the links ',
         },
         {
-            type: 'input',
-            name: 'food',
-            message: 'What is your favorite food?',
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your GitHub Username',
-        },
-        {
-            type: 'input',
-            name: 'linkedin',
-            message: 'Enter your LinkedIn URL.',
+            type: 'list',
+            message: 'Add a license',
+            choices: [
+
+            ],
+            name: 'license',
         },
     ])
-    .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
 
-        fs.writeFile('index.html', htmlPageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html!')
-        );
-    });
+    const init = () => {
+
+        userInfo()
+            .then((answers) => fs.writeFileSync("./README.md", generateReadme(answers)))
+            .then(() => console.log("Congratulations you wrote to ReadMe.md!"))
+            .catch((err) => console.error(err));
+    };
+
+    init();
