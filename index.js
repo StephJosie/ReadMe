d5c50f4f8d0938fc4e792407f9b7ad2aa6d9dd2a
steph <sjapodaca1103@gmail.com>
2021-11-13 17:05:35 -0800

    added license and badge

--- index.js
+++ index.js
@@ -34,18 +34,53 @@ const userInfo = () => {
             type: 'list',
             message: 'Add a license',
             choices: [
+                'The Do What the Fuck You Want to Public License',
+                'The Unlicense',
+                'Eclipse Public License 1.0',
+                'Boost Software License 1.0',
+                'Apache 2.0 License',
+                'The Hippocratic License 3.0',
+                'The MIT License',
+
+
 
             ],
             name: 'license',
         },
-    ])
+        {
+            type: 'input',
+            name: 'Contribute',
+            message: 'How can other developers contribute to your project?',
+        },
+        {
+            type: 'input',
+            name: 'Tests',
+            message: 'How can other developers test your project? Provide example.',
+        },
+    ]);
+};
 
-    const init = () => {
+const badges = {
+    'The Do What the Fuck You Want to Public License': '(https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)',
+    'The Unlicense': '(https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)',
+    'Eclipse Public License 1.0': '(https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)',
+    'Boost Software License 1.0': '(https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)',
+    'Apache 2.0 License': '(https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    'The Hippocratic License 3.0': '(https://img.shields.io/badge/License-Hippocratic_3.0-lightgrey.svg)](https://firstdonoharm.dev)',
+    'The MIT License': '(https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
 
-        userInfo()
-            .then((answers) => fs.writeFileSync("./README.md", generateReadme(answers)))
-            .then(() => console.log("Congratulations you wrote to ReadMe.md!"))
-            .catch((err) => console.error(err));
-    };
 
-    init();
+
+
+
+}
+
+const init = () => {
+
+    userInfo()
+        .then((answers) => fs.writeFileSync("./README.md", generateReadme(answers)))
+        .then(() => console.log("Congratulations you wrote to ReadMe.md!"))
+        .catch((err) => console.error(err));
+};
+
+init();
